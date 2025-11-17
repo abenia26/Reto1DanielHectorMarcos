3eaf96e7860944bf8a7f58459ed00341fbd45c80
MVM030 <1DAM@A11-PC20>
2025-11-17 13:00:36 +0100

    Merge branch 'main' of https://github.com/abenia26/Reto1DanielHectorMarcos.git into main

--- Reto1DanielHectorMarcos/src/reto/Funciones3.java
+++ Reto1DanielHectorMarcos/src/reto/Funciones3.java
@@ -1,9 +1,12 @@
 package reto;
 
 public class Funciones3 {
+public static int suma(int n1, int n2) {
+		return n1+n2;
+}
+public static int resta(int n1, int n2) {
+	return n1-n2;
+}
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
+	
 }
