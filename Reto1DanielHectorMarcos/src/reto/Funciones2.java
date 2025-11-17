ee27faed7c49f4edd46ad33d62c887f760589ab8
tegocalderonn <1DAM@A11-PC19>
2025-11-17 11:54:50 +0100

    Funcion es par o es impar

--- Reto1DanielHectorMarcos/src/reto/Funciones2.java
+++ Reto1DanielHectorMarcos/src/reto/Funciones2.java
@@ -2,9 +2,18 @@ package reto;
 
 public class Funciones2 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static boolean esPar(int n) {
+		if (n % 2 == 0) {
+			return true;
+		} else {
+			return false;
+		}
 	}
-
+	public static boolean esImpar(int n) {
+		if (n % 2 != 0) {
+			return true;
+		} else {
+			return false;
+		}
+	}
 }
