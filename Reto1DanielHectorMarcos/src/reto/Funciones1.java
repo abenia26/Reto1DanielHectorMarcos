751b5c677ab13574589bbd9a7445198918f382ae
abenia26 <1DAM@A11-PC27>
2025-11-17 13:01:26 +0100

    funciones positivo,negativo,multiplicar y dividir

--- Reto1DanielHectorMarcos/src/reto/Funciones1.java
+++ Reto1DanielHectorMarcos/src/reto/Funciones1.java
@@ -1,10 +1,24 @@
 package reto;
 
 public class Funciones1 {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+public static boolean esPositivo(int n) {
+	if(n>0) {
+		return true;
+	}else {
+		return false;
 	}
-
+}
+public static boolean esNegativo(int n) {
+	if(n<0) {
+		return true;
+	}else {
+		return false;
+	}
+}
+public static int multiplicacion(int n1,int n2) {
+	return n1*n2;
+}
+public static int division(int n1,int n2) {
+	return n1/n2;
+}
 }
